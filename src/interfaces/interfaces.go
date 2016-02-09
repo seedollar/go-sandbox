12e97e161b63f30a946e0674f43d917a32d043ea
Oliver Loe <oliver.loe@fnb.co.za>
2016-02-09 07:47:52 +0200

    Updated interfaces.go

--- src/interfaces/interfaces.go
+++ src/interfaces/interfaces.go
@@ -22,16 +22,23 @@ func (female Female) GetGender() string {
 	return female.gender
 }
 
+func describeInteface(person Person) {
+	fmt.Printf("\n(%v, %T)", person, person)
+}
+
 func main() {
 	var person1, person2 Person
 	guy := Male{"male"}
 	chic := Female{"female"}
 
-	person1 = &guy
-	person2 = &chic
+	person1 = guy
+	person2 = chic
 
 	fmt.Println(person1.GetGender())
 	fmt.Println(person2.GetGender())
+	// print interface tuple
+	describeInteface(person1)
+	describeInteface(person2)
 
 }
 
